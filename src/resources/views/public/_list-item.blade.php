039294d9c51dc4dee0606e57f2e6c66968157bbb
Samuel De Backer <sdebacker@gmail.com>
2015-11-27 15:32:30 +0100

    view

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,5 +1,3 @@
-<div class="swiper-slide">
-    <a href="{{ $slide->page->uri($lang) }}" title="{{ $slide->title }}" target="_blank">
-        {!! $slide->present()->thumb(1140, 500) !!}
-    </a>
-</div>
+<a class="swiper-slide" style="background: url({!! $slide->present()->thumbSrc(1140, 500) !!})" href="{{ $slide->page->uri($lang) }}" title="{{ $slide->title }}" target="_blank">
+    <div class="slider-text">{!! $slide->present()->body !!}</div>
+</a>
