4f10d39bfd851f0ba400b78446611a1cc4ce75ef
Samuel De Backer <sdebacker@gmail.com>
2015-12-30 15:54:00 +0100

    admin controller

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace TypiCMS\Modules\Slides\Http\Controllers;
 
 use TypiCMS\Modules\Core\Http\Controllers\BaseAdminController;
 use TypiCMS\Modules\Slides\Http\Requests\FormRequest;
+use TypiCMS\Modules\Slides\Models\Slide;
 use TypiCMS\Modules\Slides\Repositories\SlideInterface;
 
 class AdminController extends BaseAdminController
@@ -14,11 +15,37 @@ class AdminController extends BaseAdminController
     }
 
     /**
+     * Create form for a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        $model = $this->repository->getModel();
+
+        return view('core::admin.create')
+            ->with(compact('model'));
+    }
+
+    /**
+     * Edit form for the specified resource.
+     *
+     * @param \TypiCMS\Modules\Slides\Models\Slide $slide
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit(Slide $slide)
+    {
+        return view('core::admin.edit')
+            ->with(['model' => $slide]);
+    }
+
+    /**
      * Store a newly created resource in storage.
      *
-     * @param FormRequest $request
+     * @param \TypiCMS\Modules\Slides\Http\Requests\FormRequest $request
      *
-     * @return Redirect
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(FormRequest $request)
     {
@@ -30,15 +57,29 @@ class AdminController extends BaseAdminController
     /**
      * Update the specified resource in storage.
      *
-     * @param  $model
-     * @param FormRequest $request
+     * @param \TypiCMS\Modules\Slides\Models\Slide              $slide
+     * @param \TypiCMS\Modules\Slides\Http\Requests\FormRequest $request
      *
-     * @return Redirect
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update($model, FormRequest $request)
+    public function update(Slide $slide, FormRequest $request)
     {
         $this->repository->update($request->all());
 
-        return $this->redirect($request, $model);
+        return $this->redirect($request, $slide);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \TypiCMS\Modules\Slides\Models\Slide $slide
+     *
+     * @return \Illuminate\Http\RedirectResponse|null
+     */
+    public function destroy(Slide $slide)
+    {
+        if ($this->repository->delete($slide)) {
+            return back();
+        }
     }
 }
