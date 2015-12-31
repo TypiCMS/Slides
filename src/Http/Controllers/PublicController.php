47cd790014af15206da484669ae96f17989f8b06
Samuel De Backer <sdebacker@gmail.com>
2015-12-31 11:49:39 +0100

    create and edit methods

--- src/Http/Controllers/PublicController.php
+++ src/Http/Controllers/PublicController.php
@@ -15,7 +15,7 @@ class PublicController extends BasePublicController
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Support\Facades\Response
+     * @return \Illuminate\View\View
      */
     public function index()
     {
@@ -28,7 +28,7 @@ class PublicController extends BasePublicController
     /**
      * Show news.
      *
-     * @return \Illuminate\Support\Facades\Response
+     * @return \Illuminate\View\View
      */
     public function show($slug)
     {
