f1dbb9e274a3a9ba79ac2e1a3dc0e5ff2ce375f1
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 12:16:45 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name'     => 'Slides',
+    'slides'   => 'slide|slides',
+    'New'      => 'Nuevo slide',
+    'Edit'     => 'Editar slide',
+    'Back'     => 'Volver a los slides',
+];
