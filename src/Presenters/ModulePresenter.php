fd7dce1a908bd23e6909e14dc4b5415aea6c07a6
Samuel De Backer <sdebacker@gmail.com>
2016-04-13 15:50:58 +0200

    edit slide title

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -19,7 +19,7 @@ class ModulePresenter extends Presenter
 
     public function title()
     {
-        return 'Slide '.$this->id;
+        return trans('slides::global.edit');
     }
 
     public function title_attribute()
