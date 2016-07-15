7116dba8f3e55fe8393c0bfe819c39df3bde9ef3
Samuel De Backer <sdebacker@gmail.com>
2016-07-15 12:41:16 +0200

    page_id can be null

--- src/database/migrations/2015_11_27_130000_create_slides_table.php
+++ src/database/migrations/2015_11_27_130000_create_slides_table.php
@@ -16,7 +16,7 @@ class CreateSlidesTable extends Migration
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->integer('position')->unsigned()->default(1);
-            $table->integer('page_id')->unsigned();
+            $table->integer('page_id')->unsigned()->nullable();
             $table->string('image')->nullable();
             $table->string('url')->nullable();
             $table->timestamps();
