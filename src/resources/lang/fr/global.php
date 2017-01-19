84e33d453bd744871bf247509815068cd6a08f28
Samuel De Backer <sdebacker@gmail.com>
2017-01-19 10:02:34 +0100

    array alignment removed

--- src/resources/lang/fr/global.php
+++ src/resources/lang/fr/global.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'name'     => 'Slides',
-    'slides'   => 'slide|slides',
-    'New'      => 'Nouveau slide',
-    'Edit'     => 'Modifier le slide',
-    'Back'     => 'Retour à la liste des slides',
+    'name' => 'Slides',
+    'slides' => 'slide|slides',
+    'New' => 'Nouveau slide',
+    'Edit' => 'Modifier le slide',
+    'Back' => 'Retour à la liste des slides',
 ];
