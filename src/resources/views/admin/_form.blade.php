c05d5167c1dde0740a69fdd5adbb8cd8c32907b2
Samuel De Backer <sdebacker@gmail.com>
2017-01-20 18:22:58 +0100

    validations attributes in a json file

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -10,18 +10,18 @@
 
 <div class="row">
     <div class="col-sm-2 form-group @if($errors->has('position'))has-error @endif">
-        {!! BootForm::text(__('validation.attributes.position'), 'position') !!}
+        {!! BootForm::text(__('Position'), 'position') !!}
     </div>
 </div>
 
 <div class="row">
     <div class="col-sm-6">
-        {!! BootForm::text(__('validation.attributes.url'), 'url') !!}
+        {!! BootForm::text(__('Url'), 'url') !!}
     </div>
 </div>
 
-{!! BootForm::select(__('validation.attributes.page_id'), 'page_id', Pages::allForSelect()) !!}
+{!! BootForm::select(__('Page'), 'page_id', Pages::allForSelect()) !!}
 
 {!! TranslatableBootForm::hidden('status')->value(0) !!}
-{!! TranslatableBootForm::checkbox(__('validation.attributes.online'), 'status') !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.body'), 'body')->addClass('ckeditor') !!}
+{!! TranslatableBootForm::checkbox(__('Online'), 'status') !!}
+{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor') !!}
