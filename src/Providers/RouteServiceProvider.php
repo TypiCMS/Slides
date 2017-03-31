63b0e247d3465bfa63f7c7f54d5b2d01ac35e2ee
Samuel De Backer <sdebacker@gmail.com>
2017-03-31 22:20:31 +0200

    Duplicate route name

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -36,7 +36,7 @@ class RouteServiceProvider extends ServiceProvider
                 $router->get('slides/{slide}/edit', 'AdminController@edit')->name('admin::edit-slide');
                 $router->post('slides', 'AdminController@store')->name('admin::store-slide');
                 $router->put('slides/{slide}', 'AdminController@update')->name('admin::update-slide');
-                $router->patch('slides/{ids}', 'AdminController@ajaxUpdate')->name('admin::update-slide');
+                $router->patch('slides/{ids}', 'AdminController@ajaxUpdate')->name('admin::update-slide-ajax');
                 $router->delete('slides/{ids}', 'AdminController@destroyMultiple')->name('admin::destroy-slide');
             });
         });
