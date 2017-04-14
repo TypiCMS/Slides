98b0ca5bb7545da1e2601b302fa48076de065644
Samuel De Backer <sdebacker@gmail.com>
2017-04-14 13:27:55 +0200

    permissions in config

---
+++ src/config/permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'slides' => [
+        'index-slides' => 'Show list',
+        'create-slide' => 'Create',
+        'store-slide' => 'Store',
+        'edit-slide' => 'Edit',
+        'update-slide' => 'Update',
+        'destroy-slide' => 'Destroy',
+    ],
+];
