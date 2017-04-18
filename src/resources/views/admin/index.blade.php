245a65d1902ebc54a44f393a07f057ed3fd7f099
Samuel De Backer <sdebacker@gmail.com>
2017-04-18 23:02:08 +0200

    class status

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -23,7 +23,7 @@
                 <tr>
                     <th class="delete"></th>
                     <th class="edit"></th>
-                    <th st-sort="status_translated" class="status_translated st-sort">{{ __('Status') }}</th>
+                    <th st-sort="status_translated" class="status st-sort">{{ __('Status') }}</th>
                     <th st-sort="image" class="image st-sort">{{ __('Image') }}</th>
                     <th st-sort="position" st-sort-default="true" class="position st-sort">{{ __('Position') }}</th>
                     <th>{{ __('Body') }}</th>
