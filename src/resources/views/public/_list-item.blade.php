9bf7f8a71a5886ed7877a5866eb576ce6b4530cb
Samuel De Backer <sdebacker@gmail.com>
2017-12-22 13:11:53 +0100

    rel="noopener noreferrer" added

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,3 +1,3 @@
-<a class="swiper-slide" style="background: url({!! $slide->present()->thumbSrc(1500, 800) !!})" href="{{ $slide->present()->url }}" title="{{ $slide->present()->title_attribute }}" @if ($slide->url) target="_blank"@endif>
+<a class="swiper-slide" style="background: url({!! $slide->present()->thumbSrc(1500, 800) !!})" href="{{ $slide->present()->url }}" title="{{ $slide->present()->title_attribute }}" @if ($slide->url) target="_blank" rel="noopener noreferrer"@endif>
     <div class="slider-text">{!! $slide->present()->body !!}</div>
 </a>
