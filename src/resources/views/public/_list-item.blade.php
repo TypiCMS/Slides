d100b3da68e1feb54dfc9fe4939ef6bfd9647530
Samuel De Backer <sdebacker@gmail.com>
2017-12-28 14:15:16 +0100

    background-image

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,3 +1,3 @@
-<a class="swiper-slide" style="background: url({!! $slide->present()->thumbSrc(1500, 800) !!})" href="{{ $slide->present()->url }}" title="{{ $slide->present()->title_attribute }}" @if ($slide->url) target="_blank" rel="noopener noreferrer"@endif>
+<a class="swiper-slide" style="background-image: url({!! $slide->present()->thumbSrc(1500, 800) !!})" href="{{ $slide->present()->url }}" title="{{ $slide->present()->title_attribute }}" @if ($slide->url) target="_blank" rel="noopener noreferrer"@endif>
     <div class="slider-text">{!! $slide->present()->body !!}</div>
 </a>
