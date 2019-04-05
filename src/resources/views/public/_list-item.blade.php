62b2494ce14f97eb4b3a1728834aa87ceba0231e
Samuel De Backer <sdebacker@gmail.com>
2019-04-05 23:49:15 +0200

    thumb() method replaced by image()

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,3 +1,3 @@
-<a class="slide-list-item swiper-slide" style="background-image: url({!! $slide->present()->thumbSrc(1500, 800) !!})" href="{{ $slide->present()->url }}" title="{{ $slide->present()->title_attribute }}" @if ($slide->url) target="_blank" rel="noopener noreferrer"@endif>
+<a class="slide-list-item swiper-slide" style="background-image: url({!! $slide->present()->image(1500, 800) !!})" href="{{ $slide->present()->url }}" title="{{ $slide->present()->title_attribute }}" @if ($slide->url) target="_blank" rel="noopener noreferrer"@endif>
     <div class="slide-list-item-text slider-text">{!! $slide->present()->body !!}</div>
 </a>
