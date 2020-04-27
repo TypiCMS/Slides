c017df6c7f2944f67689320a268aefbb0a55fbd5
Samuel De Backer <sdebacker@gmail.com>
2020-04-27 13:43:25 +0200

    Update _slider.blade.php

--- src/resources/views/public/_slider.blade.php
+++ src/resources/views/public/_slider.blade.php
@@ -1,4 +1,4 @@
-<div class="carrousel container-slides {{ $items->count() > 1 ? 'swiper-container' : '' }}">
+<div class="container-slides {{ $items->count() > 1 ? 'carrousel' : '' }}">
     @include('slides::public._list')
     @if ($items->count() > 1)
     <div class="carrousel-pagination swiper-pagination"></div>
