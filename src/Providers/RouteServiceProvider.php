bf45055adf711e876f5d3308453ddddf2ac53915
Samuel De Backer <sdebacker@gmail.com>
2020-09-17 23:51:39 +0200

    Callable syntax for controller route declarations.

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -5,18 +5,11 @@ namespace TypiCMS\Modules\Slides\Providers;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+use TypiCMS\Modules\Slides\Http\Controllers\AdminController;
+use TypiCMS\Modules\Slides\Http\Controllers\ApiController;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to the controller routes in your routes file.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'TypiCMS\Modules\Slides\Http\Controllers';
-
     /**
      * Define the routes for the application.
      */
@@ -27,11 +20,11 @@ class RouteServiceProvider extends ServiceProvider
              * Admin routes
              */
             $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
-                $router->get('slides', 'AdminController@index')->name('admin::index-slides')->middleware('can:read slides');
-                $router->get('slides/create', 'AdminController@create')->name('admin::create-slide')->middleware('can:create slides');
-                $router->get('slides/{slide}/edit', 'AdminController@edit')->name('admin::edit-slide')->middleware('can:update slides');
-                $router->post('slides', 'AdminController@store')->name('admin::store-slide')->middleware('can:create slides');
-                $router->put('slides/{slide}', 'AdminController@update')->name('admin::update-slide')->middleware('can:update slides');
+                $router->get('slides', [AdminController::class, 'index'])->name('admin::index-slides')->middleware('can:read slides');
+                $router->get('slides/create', [AdminController::class, 'create'])->name('admin::create-slide')->middleware('can:create slides');
+                $router->get('slides/{slide}/edit', [AdminController::class, 'edit'])->name('admin::edit-slide')->middleware('can:update slides');
+                $router->post('slides', [AdminController::class, 'store'])->name('admin::store-slide')->middleware('can:create slides');
+                $router->put('slides/{slide}', [AdminController::class, 'update'])->name('admin::update-slide')->middleware('can:update slides');
             });
 
             /*
@@ -39,9 +32,9 @@ class RouteServiceProvider extends ServiceProvider
              */
             $router->middleware('api')->prefix('api')->group(function (Router $router) {
                 $router->middleware('auth:api')->group(function (Router $router) {
-                    $router->get('slides', 'ApiController@index')->middleware('can:read slides');
-                    $router->patch('slides/{slide}', 'ApiController@updatePartial')->middleware('can:update slides');
-                    $router->delete('slides/{slide}', 'ApiController@destroy')->middleware('can:delete slides');
+                    $router->get('slides', [ApiController::class, 'index'])->middleware('can:read slides');
+                    $router->patch('slides/{slide}', [ApiController::class, 'updatePartial'])->middleware('can:update slides');
+                    $router->delete('slides/{slide}', [ApiController::class, 'destroy'])->middleware('can:delete slides');
                 });
             });
         });
