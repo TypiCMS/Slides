f2e42859a8e81bb51cfd82a3dc49f81b017dff38
Samuel De Backer <sdebacker@gmail.com>
2022-04-19 21:36:51 +0200

    Forms

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -3,24 +3,32 @@
     <script src="{{ asset('components/ckeditor4/config-full.js') }}"></script>
 @endpush
 
-@component('core::admin._buttons-form', ['model' => $model])
-@endcomponent
+<div class="header">
+    @include('core::admin._button-back', ['url' => $model->indexUrl(), 'title' => __('Slides')])
+    @include('core::admin._title', ['default' => __('New slide')])
+    @component('core::admin._buttons-form', ['model' => $model])
+    @endcomponent
+</div>
 
-{!! BootForm::hidden('id') !!}
+<div class="content">
 
-<file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
-<file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+    {!! BootForm::hidden('id') !!}
 
-<div class="row gx-3">
-    <div class="col-sm-6">
-        {!! BootForm::text(__('Url'), 'url')->type('url')->placeholder('https://') !!}
+    <file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
+    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+
+    <div class="row gx-3">
+        <div class="col-sm-6">
+            {!! BootForm::text(__('Url'), 'url')->type('url')->placeholder('https://') !!}
+        </div>
     </div>
-</div>
 
-{!! BootForm::select(__('Page'), 'page_id', Pages::allForSelect()) !!}
+    {!! BootForm::select(__('Page'), 'page_id', Pages::allForSelect()) !!}
 
-<div class="mb-3">
-    {!! TranslatableBootForm::hidden('status')->value(0) !!}
-    {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+    <div class="mb-3">
+        {!! TranslatableBootForm::hidden('status')->value(0) !!}
+        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+    </div>
+    {!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
+
 </div>
-{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -4,11 +4,6 @@
 
 @section('content')
 
-    <div class="header">
-        @include('core::admin._button-back', ['module' => 'slides'])
-        <h1 class="header-title">@lang('New slide')</h1>
-    </div>
-
     {!! BootForm::open()->action(route('admin::index-slides'))->multipart()->role('form') !!}
         @include('slides::admin._form')
     {!! BootForm::close() !!}
