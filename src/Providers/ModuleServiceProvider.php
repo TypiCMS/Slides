85316e9e930ccfe9519a9c9eb4cac1c300bc21fa
Samuel De Backer <sdebacker@gmail.com>
2023-04-17 11:40:15 +0200

    concat space

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -14,13 +14,13 @@ class ModuleServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/slides.php', 'typicms.modules.slides');
+        $this->mergeConfigFrom(__DIR__ . '/../config/slides.php', 'typicms.modules.slides');
 
-        $this->loadViewsFrom(__DIR__.'/../../resources/views/', 'slides');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views/', 'slides');
 
-        $this->publishes([__DIR__.'/../../database/migrations/create_slides_table.php.stub' => getMigrationFileName('create_slides_table')], 'typicms-migrations');
-        $this->publishes([__DIR__.'/../../resources/views' => resource_path('views/vendor/slides')], 'typicms-views');
-        $this->publishes([__DIR__.'/../../resources/scss' => resource_path('scss')], 'typicms-resources');
+        $this->publishes([__DIR__ . '/../../database/migrations/create_slides_table.php.stub' => getMigrationFileName('create_slides_table')], 'typicms-migrations');
+        $this->publishes([__DIR__ . '/../../resources/views' => resource_path('views/vendor/slides')], 'typicms-views');
+        $this->publishes([__DIR__ . '/../../resources/scss' => resource_path('scss')], 'typicms-resources');
 
         AliasLoader::getInstance()->alias('Slides', Slides::class);
 
