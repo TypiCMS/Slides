0aaff64726cfb099fff7bae157731f44a7b7fc49
Samuel De Backer <sdebacker@gmail.com>
2023-06-28 15:49:38 +0200

    prettier

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,53 +4,23 @@
 
 @section('content')
 
-<item-list
-    url-base="/api/slides"
-    fields="id,image_id,position,status,body"
-    table="slides"
-    title="slides"
-    include="image"
-    :searchable="['body']"
-    :sorting="['position']"
->
+<item-list url-base="/api/slides" fields="id,image_id,position,status,body" table="slides" title="slides" include="image" :searchable="['body']" :sorting="['position']">
     <template slot="add-button" v-if="$can('create slides')">
         @include('core::admin._button-create', ['module' => 'slides'])
     </template>
 
     <template slot="columns" slot-scope="{ sortArray }">
-        <item-list-column-header
-            name="checkbox"
-            v-if="$can('update slides')||$can('delete slides')"
-        ></item-list-column-header>
+        <item-list-column-header name="checkbox" v-if="$can('update slides')||$can('delete slides')"></item-list-column-header>
         <item-list-column-header name="edit" v-if="$can('update slides')"></item-list-column-header>
-        <item-list-column-header
-            name="status_translated"
-            sortable
-            :sort-array="sortArray"
-            :label="$t('Status')"
-        ></item-list-column-header>
-        <item-list-column-header
-            name="position"
-            sortable
-            :sort-array="sortArray"
-            :label="$t('Position')"
-        ></item-list-column-header>
+        <item-list-column-header name="status_translated" sortable :sort-array="sortArray" :label="$t('Status')"></item-list-column-header>
+        <item-list-column-header name="position" sortable :sort-array="sortArray" :label="$t('Position')"></item-list-column-header>
         <item-list-column-header name="image" :label="$t('Image')"></item-list-column-header>
-        <item-list-column-header
-            name="body_translated"
-            sortable
-            :sort-array="sortArray"
-            :label="$t('Content')"
-        ></item-list-column-header>
+        <item-list-column-header name="body_translated" sortable :sort-array="sortArray" :label="$t('Content')"></item-list-column-header>
     </template>
 
     <template slot="table-row" slot-scope="{ model, checkedModels, loading }">
         <td class="checkbox" v-if="$can('update slides')||$can('delete slides')">
-            <item-list-checkbox
-                :model="model"
-                :checked-models-prop="checkedModels"
-                :loading="loading"
-            ></item-list-checkbox>
+            <item-list-checkbox :model="model" :checked-models-prop="checkedModels" :loading="loading"></item-list-checkbox>
         </td>
         <td v-if="$can('update slides')">
             <item-list-edit-button :url="'/admin/slides/'+model.id+'/edit'"></item-list-edit-button>
