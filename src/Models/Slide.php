b1a0a0406eae7888758935b47f74461a4e72cfd2
Samuel De Backer <sdebacker@gmail.com>
2025-06-06 13:08:27 +0200

    PHPDoc

--- src/Models/Slide.php
+++ src/Models/Slide.php
@@ -3,6 +3,7 @@
 namespace TypiCMS\Modules\Slides\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
 use Laracasts\Presenter\PresentableTrait;
@@ -11,6 +12,7 @@ use Spatie\EloquentSortable\SortableTrait;
 use Spatie\Translatable\HasTranslations;
 use TypiCMS\Modules\Core\Models\Base;
 use TypiCMS\Modules\Core\Models\File;
+use TypiCMS\Modules\Core\Models\History;
 use TypiCMS\Modules\Core\Models\Page;
 use TypiCMS\Modules\Core\Traits\Historable;
 use TypiCMS\Modules\Slides\Presenters\ModulePresenter;
@@ -21,6 +23,16 @@ use TypiCMS\Modules\Slides\Presenters\ModulePresenter;
  * @property-read string $thumb
  * @property-read Carbon $created_at
  * @property-read Carbon $updated_at
+ * @property int $position
+ * @property int|null $image_id
+ * @property string|null $website
+ * @property array<array-key, mixed> $status
+ * @property array<array-key, mixed> $body
+ * @property-read Collection<int, History> $history
+ * @property-read int|null $history_count
+ * @property-read File|null $image
+ * @property-read Page|null $page
+ * @property-read mixed $translations
  */
 class Slide extends Base implements Sortable
 {
@@ -51,11 +63,13 @@ class Slide extends Base implements Sortable
         );
     }
 
+    /** @return BelongsTo<Page, $this> */
     public function page(): BelongsTo
     {
         return $this->belongsTo(Page::class);
     }
 
+    /** @return BelongsTo<File, $this> */
     public function image(): BelongsTo
     {
         return $this->belongsTo(File::class, 'image_id');
