0d927307b9b0c15a823de085f1d96d3397097293
Samuel De Backer <sdebacker@gmail.com>
2025-10-16 13:06:17 +0200

    Update _form.blade.php

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -7,20 +7,27 @@
 <div class="content">
     <x-core::form-errors />
 
-    <file-manager></file-manager>
-    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="row gx-3">
+                <div class="col-sm-6">
+                    {!! BootForm::text(__('Website'), 'website')->type('url')->placeholder('https://') !!}
+                </div>
+            </div>
 
-    <div class="row gx-3">
-        <div class="col-sm-6">
-            {!! BootForm::text(__('Website'), 'website')->type('url')->placeholder('https://') !!}
+            {!! BootForm::select(__('Page'), 'page_id', Pages::allForSelect()) !!}
+
+            <div class="mb-3">
+                {!! TranslatableBootForm::hidden('status')->value(0) !!}
+                {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+            </div>
+            {!! TranslatableBootForm::textarea(__('Body'), 'body') !!}
+        </div>
+        <div class="col-lg-4">
+            <div class="right-column">
+                <file-manager></file-manager>
+                <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+            </div>
         </div>
     </div>
-
-    {!! BootForm::select(__('Page'), 'page_id', Pages::allForSelect()) !!}
-
-    <div class="mb-3">
-        {!! TranslatableBootForm::hidden('status')->value(0) !!}
-        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
-    </div>
-    {!! TranslatableBootForm::textarea(__('Body'), 'body') !!}
 </div>
