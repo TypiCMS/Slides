d3f1351db0ce8ec56fab949681807a2be0ca881b
Samuel De Backer <sdebacker@gmail.com>
2025-11-02 20:25:31 +0100

    Validation rules specified as arrays.

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -6,17 +6,17 @@ use TypiCMS\Modules\Core\Http\Requests\AbstractFormRequest;
 
 class FormRequest extends AbstractFormRequest
 {
-    /** @return array<string, string> */
+    /** @return array<string, list<string>> */
     public function rules(): array
     {
         return [
-            'website' => 'nullable|url|max:255',
-            'image_id' => 'nullable|integer',
-            'page_id' => 'nullable|integer',
-            'title.*' => 'nullable|max:255',
-            'slug.*' => 'nullable|alpha_dash|max:255|required_if:status.*,1|required_with:title.*',
-            'status.*' => 'boolean',
-            'body.*' => 'nullable|max:20000',
+            'website' => ['nullable', 'url', 'max:255'],
+            'image_id' => ['nullable', 'integer'],
+            'page_id' => ['nullable', 'integer'],
+            'title.*' => ['nullable', 'max:255'],
+            'slug.*' => ['nullable', 'alpha_dash', 'max:255', 'required_if:status.*,1', 'required_with:title.*'],
+            'status.*' => ['boolean'],
+            'body.*' => ['nullable', 'max:20000'],
         ];
     }
 }
